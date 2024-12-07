a0aca6a01b1b6039e9eaf3baf737ca723659b62e
Mauro-100km <mauro.100km@gmail.com>
2024-12-06 21:11:11 -0300

    Criei um doc sobre identação e um projeto praticando

---
+++ Calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,13 @@
+package calculadora;
+
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+        int a =3;
+        int b=5;
+        int soma = a+b; 
+       System.out.println(soma); 
+    }
+    
+}
